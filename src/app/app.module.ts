8100c26c2bd8fb96d8b2b81ca2cf974004f8c500
Karishma Gupta <guptakarishma04@gmail.com>
2017-10-10 11:54:20 +0530

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent }  from './app.component';
+import { EventsAppComponent } from './events-app.component'
+import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+  bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {}
